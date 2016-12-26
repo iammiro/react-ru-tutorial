61a12c9291cb4b79e97ec27610db69ffe5ee0e88
iammiro <miroslav.kolomiets@gmail.com>
2016-12-26 15:46:28 +0200

    Add react project

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const News = React.createClass({
+    render: function() {
+        return (
+            <div className="news">
+                К сожалению, новостей нет.
+            </div>
+        );
+    }
+});
+
+const Comments = React.createClass({
+    render: function() {
+        return (
+            <div className="comments">
+                Нет новостей - комментировать нечего.
+            </div>
+        );
+    }
+});
+
+const App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                Всем привет, я компонент App! Я умею отображать новости.
+                <News />
+                <Comments />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);

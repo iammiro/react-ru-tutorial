3ccf29dc5bd449aafc246735da63d2e01abea15c
iammiro <miroslav.kolomiets@gmail.com>
2016-12-26 18:18:01 +0200

    Add styles

--- js/app.js
+++ js/app.js
@@ -10,26 +10,35 @@ const my_news = [
     {
         author: 'Гость',
         text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    },
+    {
+        author: 'UFO',
+        text: 'Test'
     }
 ];
 
 const News = React.createClass({
     render: function() {
-        var data = this.props.data;
-        var newsTemplate = data.map(function(item, index) {
-            return (
-                <div key={index}>
-                    <p className="news__author">{item.author}:</p>
-                    <p className="news__text">{item.text}</p>
-                </div>
-            )
-        })
+        let data = this.props.data;
+        let newsTemplate;
 
-        console.log(newsTemplate);
+        if (data.length > 0) {
+            newsTemplate = data.map(function(item, index) {
+                return (
+                    <div key={index}>
+                        <p className="news__author">{item.author}:</p>
+                        <p className="news__text">{item.text}</p>
+                    </div>
+                )
+            })
+        } else {
+            newsTemplate = <p>К сожалению новостей нет</p>
+        }
 
         return (
             <div className="news">
                 {newsTemplate}
+                <strong className={data.length > 0 ? '':'none'}>Всего новостей: {data.length}</strong>
             </div>
         );
     }
